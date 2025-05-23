e1e80ad0e5b4a6bcae4b4f451a6920a0a17352e6
Sebastian <sebastianrme87@gmail.com>
2025-05-22 23:30:31 -0500

    cambios en listar redes perteciente

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,6 +1,5 @@
 import { db } from '../config/firebase-admin.js';
 import Usuario from '../models/usuario.model.js';
-import { db } from '../config/firebase-admin.js';
 import Usuario from '../models/usuario.model.js';
 import Red from '../models/red.model.js'; // Asegúrate de que esta importación sea correcta
 
