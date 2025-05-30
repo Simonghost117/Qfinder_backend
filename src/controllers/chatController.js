45831da102f57b20006f3e3680247c9a8d339ef6
Sebastian <sebastianrme87@gmail.com>
2025-05-30 18:40:46 -0500

    logica para recibir notificaciones11

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -173,7 +173,7 @@ const enviarNotificacionesPush = async ({ comunidadId, remitenteId, mensajeId, m
       include: [{
         model: Usuario,
         as: 'usuario', // Usando el alias definido en la asociación
-        attributes: ['id_usuario', 'fcm_token', 'notificaciones_activas']
+        attributes: ['id_usuario', 'fcm_token']
       }]
     });
 
