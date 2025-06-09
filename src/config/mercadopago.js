f6c37d1de0a453a39599d47a3b8782a6bdb18e97
Sebastian <sebastianrme87@gmail.com>
2025-06-08 19:25:02 -0500

    checkout pro mercado pago

--- src/config/mercadopago.js
+++ src/config/mercadopago.js
@@ -1,7 +1,7 @@
 import { MercadoPagoConfig } from 'mercadopago';
-const tokent = "TEST-358197303018633-060512-ccf1663185081e779360ba7d539ce364-390857873"
+
 export const configureMercadoPago = () => {
-  const accessToken = tokent || process.env.MERCADOPAGO_ACCESS_TOKEN;
+  const accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
   
   if (!accessToken) {
     throw new Error('MERCADOPAGO_ACCESS_TOKEN no está definido');
