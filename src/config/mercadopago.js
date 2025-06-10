e13c4a644b1a7c89b7f331c015e5f22258af3a8b
Sebastian <sebastianrme87@gmail.com>
2025-06-10 15:26:55 -0500

    manejo de subscripciones mejorado6

--- src/config/mercadopago.js
+++ src/config/mercadopago.js
@@ -12,7 +12,7 @@ export const configureMercadoPago = () => {
   return new MercadoPagoConfig({
     accessToken: accessToken,
     options: {
-      timeout: 30000,
+      timeout: 50000,
       idempotencyKey: `mp-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`,
       integratorId: process.env.MERCADOPAGO_INTEGRATOR_ID,
       sandbox: isSandbox,
