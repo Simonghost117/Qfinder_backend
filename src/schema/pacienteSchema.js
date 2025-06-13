06cd6c97440cd5d79849abd01f5b12bbd14d5197
Alishon <alison.martinez@soy.sena.edu.co>
2025-06-13 07:53:53 -0500

    Solucion de errores

--- src/schema/pacienteSchema.js
+++ src/schema/pacienteSchema.js
@@ -114,3 +114,53 @@ export const ActPaciente2 = z.object({
       message: "La escritura del diagnostico no puede exceder los 100 carácteres"
     }),
 })
+
+export const PacienteSchema2 = z.object({
+  id_usuario: z.number({
+    required_error: "El ID de usuario es requerido",
+    invalid_type_error: "El ID de usuario debe ser un número"
+  })
+  .int({
+    message: "El ID de usuario debe ser un número entero"
+  })
+  .positive({
+    message: "El ID de usuario debe ser un número positivo"
+  }),
+  
+  nombre: z.string()
+    .min(2, {
+      message: "El nombre del paciente debe tener al menos 2 carácteres"
+    })
+    .max(100, {
+      message: "El nombre del paciente no puede exceder los 100 carácteres"
+    }),
+  apellido: z.string()
+    .min(2, {
+      message: "El apellido del paciente debe tener al menos 2 carácteres"
+    })
+    .max(100, {
+      message: "El apellido del paciente no puede exceder los 100 carácteres"
+    }),
+  identificacion: z.string()
+    .min(5, {
+      message: "La identificación del paciente debe tener al menos 5 carácteres"
+    }),
+  fecha_nacimiento: z.string()
+    .transform((str) => new Date(str))
+    .refine((date) => {
+      return date.getTime() <= new Date().getTime();
+    }, {
+      message: "La fecha no puede estar en el futuro.",
+    }),
+  sexo: z.enum(['masculino', 'femenino', 'otro', 'prefiero_no_decir'], {
+    message: "La orientación sexual debe ser obligatoria"
+  }),
+  diagnostico_principal: z.string()
+    .min(3, {
+      message: "El Diagnostico Principal debe tener almenos 10 carácteres"
+    })
+    .max(100, {
+      message: "La escritura del diagnostico no puede exceder los 100 carácteres"
+    }),
+  nivel_autonomia: z.enum(['alta', 'media', 'baja']).optional()
+})
