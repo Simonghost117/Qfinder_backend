332a1f36e3a242547b70f33faeb01f778965ce5c
Sebastian <sebastianrme87@gmail.com>
2025-06-16 11:49:28 -0500

    redespliegue

--- src/routes/colaborador.routes.js
+++ src/routes/colaborador.routes.js
@@ -12,7 +12,8 @@ router.get('/buscar/:correo', verifyToken, buscarUsuarioPorCorreo);
 
 router.post('/agregar', verifyToken, validatePermissions(['responsable']), validarSchema(validarAgregarColaborador), agregarColaborador);
 
-//listar todos los colaboradores de un usuario
+//listar todos los colaboradores de un usuario ddddd
+
 router.get('/mis-pacientes', verifyToken, listarColaboradoresDeMisPacientes);
 
 
