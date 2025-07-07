083a4e1c9c95a018c04984a9025a94b180885d03
Alishon <alison.martinez@soy.sena.edu.co>
2025-07-06 20:09:43 -0500

    mejoras en validaciones

--- src/schema/usuarioSchema.js
+++ src/schema/usuarioSchema.js
@@ -3,23 +3,25 @@ import { z } from 'zod';
 export const registerSchema = z.object({
     nombre_usuario: z.string()
         .min(1, { message: "El nombre es obligatorio" })
-        .max(255, { message: "El nombre no puede exceder los 255 caracteres" }),
+        .max(255, { message: "El nombre no puede exceder los 255 caracteres" })
+        .regex(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, { message: "El nombre solo debe contener letras y espacios" }),
     apellido_usuario: z.string()
         .min(1, { message: "El apellido es obligatorio" })
-        .max(255, { message: "El apellido no puede exceder los 255 caracteres" }),
+        .max(255, { message: "El apellido no puede exceder los 255 caracteres" })
+        .regex(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, { message: "El apellido solo debe contener letras y espacios" }),
     identificacion_usuario: z.string()
-  .regex(/^[1-9]\d{6,10}$/, {
-    message: "La identificación debe tener entre 7 y 11 dígitos numéricos y no puede comenzar con cero",
+        .regex(/^[1-9]\d{6,10}$/, { message: "La identificación debe tener entre 7 y 11 dígitos numéricos y no puede comenzar con cero",
   }),
     direccion_usuario: z.string()
         .min(1, { message: "La dirección es obligatoria" })
         .max(255, { message: "La dirección no puede exceder los 255 caracteres" }),
     telefono_usuario: z.string()
-    .regex(/^\d{10}$/, { message: "El teléfono debe tener exactamente 10 dígitos" }),
+        .regex(/^[1-9]\d{9}$/, { message: "El teléfono debe tener 10 dígitos y no comenzar con cero" }),
     correo_usuario: z.string()
         .email({ message: "El correo electrónico debe tener un formato válido" }),
     contrasena_usuario: z.string()
-        .min(8, { message: "La contraseña debe tener al menos 8 caracteres" }), 
+        .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
+        .regex(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).+$/, { message: "La contraseña debe contener al menos una letra mayúscula y un carácter especial" }), 
     tipo_usuario: z.enum(['Usuario', 'Medico', 'Administrador']).optional(),
     estado_usuario: z.enum(['Activo', 'Inactivo']).optional(), // Opcional porque tiene un valor por defecto
     createdAt: z.date().optional(), 
