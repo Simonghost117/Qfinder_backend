59397b51f9a81c313c0b79b4b3ed777c0cf51791
Sebastian <sebastianrme87@gmail.com>
2025-07-10 17:25:13 -0500

    se colocaron los precios reales en las subscripciones

--- src/config/subscriptions.js
+++ src/config/subscriptions.js
@@ -7,14 +7,14 @@ export const SUBSCRIPTION_LIMITS = {
 export const PLANS_MERCADOPAGO = {
   plus: {
     description: "Plan Plus - 5 pacientes y 5 colaboradores",
-    amount: 2000, // $10,000 COP
+    amount: 10000, // $10,000 COP
     currency_id: "COP",
     frequency: 1, // mensual
     frequency_type: "months"
   },
   pro: {
     description: "Plan Pro - 15 pacientes y 10 colaboradores",
-    amount: 5000, // $10,000 COP
+    amount: 30000, // $10,000 COP
     currency_id: "COP",
     frequency: 1, // mensual
     frequency_type: "months"
